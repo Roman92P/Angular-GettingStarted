c773b32375babca9cda0b6e6444a6aa28e3e59f8
Roman92P <roman.paszkow@gmail.com>
2022-10-07 12:25:03 +0200

    Added code routing

--- APM-Start/src/app/app.module.ts
+++ APM-Start/src/app/app.module.ts
@@ -7,14 +7,35 @@ import { ProductListComponent } from './products/product-list.component';
 import { ConverToSpacesPipe } from './products/shared/convert-to-spaces.pipe';
 import { StarComponent } from './shared/star.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
+import { ProductDetailGuard } from './products/product-detail.guard';
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
     ConverToSpacesPipe,
     StarComponent,
+    ProductDetailComponent,
   ],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent },
+      {
+        path: 'products/:id',
+        canActivate: [ProductDetailGuard],
+        component: ProductDetailComponent,
+      },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
+    ]),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
